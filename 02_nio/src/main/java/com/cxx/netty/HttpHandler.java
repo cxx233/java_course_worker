f88ec3c0ab93e15098960a53d5e69adecfbf5528
cxx233 <angelzhanshi1995@163.com>
2022-03-06 22:31:26 +0800

    netty 代码

---
+++ 02_nio/src/main/java/com/cxx/netty/HttpHandler.java
@@ -0,0 +1,72 @@
+package com.cxx.netty;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+import io.netty.util.ReferenceCountUtil;
+
+import java.io.UnsupportedEncodingException;
+
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.rtsp.RtspHeaders.Names.CONNECTION;
+
+/**
+ * @ClassName HttpHandler
+ * @Description 处理器
+ * @Author
+ * @Date 2022/3/6 21:49
+ */
+public class HttpHandler  extends ChannelInboundHandlerAdapter{
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            // logger.info("channelRead 流量接口请求开始，时间为{}", startTime)
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            //logger.info("接收到请求到url为{}", uri)
+            // 这里类似spring mvc 的mapping，将请求连接分发不同的处理
+            if (uri.contains("/test")) {
+                hanlerTest(fullRequest, ctx, "hello, cxx");
+            } else {
+                hanlerTest(fullRequest, ctx, "hello, other");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void hanlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx, String value) {
+        FullHttpResponse response = null;
+        try {
+            response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,
+                    Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            // logger.error("处理测试接口出错"，e);
+            response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus
+                    .NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+        }
+    }
+}
---
+++ 02_nio/src/main/java/com/cxx/netty/HttpInitializer.java
@@ -0,0 +1,25 @@
+package com.cxx.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+
+
+/**
+ * @ClassName HttpInitializer
+ * @Description TODO
+ * @Author
+ * @Date 2022/3/6 22:07
+ */
+public class HttpInitializer extends ChannelInitializer<SocketChannel> {
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline p = ch.pipeline();
+        p.addLast(new HttpServerCodec());
+//        p.addLast(new HttpServerExpectContinueHandler());
+        p.addLast(new HttpObjectAggregator(1024 * 1024));
+        p.addLast(new HttpHandler());
+    }
+}
